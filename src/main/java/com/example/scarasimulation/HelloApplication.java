3b500fc6f889ccbce5421e782ed8964adce6ff3d
Taron Sargsyan <taronsargsyanunity@gmail.com>
2023-07-02 14:55:55 +0400

    3d objects group rotation

--- src/main/java/com/example/scarasimulation/HelloApplication.java
+++ src/main/java/com/example/scarasimulation/HelloApplication.java
@@ -27,15 +27,15 @@ public class HelloApplication extends Application {
         Sphere target = new Sphere(0.2);
         Cylinder plane = new Cylinder(10, 0.1);
         Box lOne = new Box(10,1,5);
+        Cylinder lOneCylinder = new Cylinder(1, 1);
         cylinder.setMaterial(new PhongMaterial(Color.BLUE));
         plane.setMaterial(new PhongMaterial(Color.ORANGE));
         target.setMaterial(new PhongMaterial(Color.RED));
         plane.translateYProperty().set(2.5);
         cylinderDesign.translateYProperty().set(-3);
-        lOne.translateYProperty().set(-3);
         target.translateYProperty().set(-3.5);
-        lOneGroup.getChildren().add(lOne);
-        lOneGroup.getChildren().add(cylinderDesign);
+        lOneGroup.getChildren().addAll(lOne, cylinderDesign, lOneCylinder);
+
 
 
 
@@ -47,10 +47,11 @@ public class HelloApplication extends Application {
         );
 
 
-        Translate translate = new Translate(5, 0, 0);
-        lOne.getTransforms().add(translate);
+        Translate translateLOne = new Translate(5, -3, 0);
+        lOne.getTransforms().add(translateLOne);
 
-
+        Translate translateLOneCylinder = new Translate(10, -4, 0);
+        lOneCylinder.getTransforms().add(translateLOneCylinder);
 
 
         Group root = new Group();
@@ -61,37 +62,23 @@ public class HelloApplication extends Application {
         root.getChildren().add(target);
 
 
-
-
         Scene scene = new Scene(root, 1000, 1000, true);
         scene.setFill(Color.web("#4287f5"));
         scene.setCamera(camera);
+
 
         primaryStage.setScene(scene);
         primaryStage.show();
 
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.A) {
+                Rotate rotat = new Rotate(10, 0, 0, 0, Rotate.Y_AXIS);
+                lOneGroup.getTransforms().add(rotat);
 
-                Rotate rotate = new Rotate();
-                rotate.setAxis(Rotate.Y_AXIS);
-                rotate.setPivotX(0);  // X coordinate of the pivot point
-                rotate.setPivotY(0);     // Y coordinate of the pivot point
-                rotate.setPivotZ(0);
-                lOne.getTransforms().add(rotate);
-                lOne.setRotationAxis(Rotate.Y_AXIS);
-                lOne.setRotate(lOne.getRotate()-10);
 
             }else if (e.getCode() == KeyCode.D){
-                Rotate rotate = new Rotate();
-                rotate.setAxis(Rotate.Y_AXIS);
-                rotate.setPivotX(0);  // X coordinate of the pivot point
-                rotate.setPivotY(0);     // Y coordinate of the pivot point
-                rotate.setPivotZ(0);
-                lOne.getTransforms().add(rotate);
-                lOne.setRotationAxis(Rotate.Y_AXIS);
-                lOne.setRotate(lOne.getRotate()+10);
-
+                Rotate rotat = new Rotate(-10, 0, 0, 0, Rotate.Y_AXIS);
+                lOneGroup.getTransforms().add(rotat);
             }else if (e.getCode() == KeyCode.UP){
                 target.translateZProperty().set(target.getTranslateZ() + 1);
 
@@ -107,6 +94,8 @@ public class HelloApplication extends Application {
 
         });
     }
+
+
     public static void main(String[] args) {
         launch();
     }
